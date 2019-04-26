6551470639793a650b6bad8961444e947cb62274
Ishwant Singh <ishwant8@gmail.com>
2019-04-26 21:14:25 +0530

    get request structure made

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ const server = require(express);
 server.use(helmet());
 server.use(express.json());
 
+server.get("/api/reciepes", (req, res) => {
+  res.json({ message: "fleshing out request structure" });
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
